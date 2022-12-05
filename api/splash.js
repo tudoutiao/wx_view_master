4835b18ba208b00ecb4abe89c5600e00fe4d356d
liuxue <liuxue@gozap.com>
2022-12-05 16:23:43 +0800

    注意 pop-window中弹幕动画无效问题解决：禁止在显示前渲染<popup-window id="pop-window"  wx:if="{{isShow}}"></popup-window>

--- api/splash.js
+++ api/splash.js
@@ -52,7 +52,29 @@ const refreshPageInfo = (localPageIndex = 1) => {
     customData.splashs = list
 }
 
+const bulletlist = [
+    { avatar: "", desc: "开始是家里是爷爷奶奶在带他，他" },
+    { avatar: "", desc: "我晚上睡觉的时候手机放在" },
+    { avatar: "", desc: "我没有文化，孩子要说查作业" },
+    { avatar: "", desc: "孩子要上网课，跟我说手机跟我说手机" },
+    { avatar: "", desc: "家里是爷爷奶奶在带他，游戏机" },
+    { avatar: "", desc: "结尾是这样子的" },
+    { avatar: "", desc: "开始是家里是爷爷奶奶在带他，他" },
+    { avatar: "", desc: "我晚上睡觉的时候手机放在" },
+    { avatar: "", desc: "我没有文化，孩子要说查作业" },
+    { avatar: "", desc: "孩子要上网课，跟我说手机跟我说手机" },
+    { avatar: "", desc: "家里是爷爷奶奶在带他，游戏机" },
+    { avatar: "", desc: "结尾是这样子的" },
+    { avatar: "", desc: "开始是家里是爷爷奶奶在带他，他" },
+    { avatar: "", desc: "我晚上睡觉的时候手机放在" },
+    { avatar: "", desc: "我没有文化，孩子要说查作业" },
+    { avatar: "", desc: "孩子要上网课，跟我说手机跟我说手机" },
+    { avatar: "", desc: "家里是爷爷奶奶在带他，游戏机" },
+    { avatar: "", desc: "结尾是这样子的" },
+]
+
 module.exports = {
+    bulletlist:bulletlist,
     customData: customData,
     /**获取接口数据 */
     getSplashList: getSplashList,
